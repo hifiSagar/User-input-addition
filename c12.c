a4f96f0294131ec887c37ebbfe2544a19a68f0e8
SAGAR T <ssagu6661@gmail.com>
2024-08-12 12:48:43 -0400

    Add files via upload

---
+++ c12.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    int choice, a = 10, b = 40;
+        
+         printf("Enter (2) to add numbers = ");
+    scanf("%d",&choice);
+    if (choice == 2)
+    {
+        printf("addition of two numbers =%d\n", a+b);
+
+    }
+    else{
+        printf("invalid choice\n");
+        
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
